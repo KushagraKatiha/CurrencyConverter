c94313bf25df89b5ce315d6e2f9d0cf9e4985843
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-10-23 04:49:15 +0530

    useEffect did'nt work

--- src/component/Converter.jsx
+++ src/component/Converter.jsx
@@ -3,18 +3,18 @@ import "./Converter.css"
 
 function Converter({ heading, list_one, list_two }) {
     
-    let newOption = list_one[0];
     
-    const [option, setOption] = useState(newOption)
     
-    let findOption = useCallback(async ()=>{
-        let optionUpdate =document.querySelector(".option1").value
-        console.log(optionUpdate);
-        setOption(optionUpdate)
-    }, [option, setOption])
+    
+    // let findOption = useCallback(()=>{ 
+    //     let option = list_one[0];
+    //     // option = document.querySelector(".option1").value
+    //     console.log(option);
+    //     return option
+    // }, [])
 
 
-    useEffect(()=>{ findOption() }, [option, setOption, findOption])
+    // useEffect(()=>{ findOption() }, [findOption])
 
     return (
         <>
@@ -27,7 +27,7 @@ function Converter({ heading, list_one, list_two }) {
 
                 {/* Currency Change Form */}
                     <div className="form">
-                        <label htmlFor="firstip" className="firstinput">Convert from : {option}</label>
+                        <label htmlFor="firstip" className="firstinput">Convert from : </label>
                         <br />
 
                         <select className="option1">
@@ -37,10 +37,10 @@ function Converter({ heading, list_one, list_two }) {
                         </select>
                         <br />
 
-                        <label htmlFor="firstip" className="secondinput">Convert to : </label>
+                        <label  htmlFor="firstip" className="secondinput">Convert to : </label>
                         <br />
 
-                        <select  className="option2">
+                        <select className="option2">
                         {list_two.map((el, idx)=>{
                                 return <option key={idx}>{el}</option>
                             })}
